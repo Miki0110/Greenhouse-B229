415d6a3fe9f3b88667bf660cfcdedc86db32391d
gsvend20 <gsvend20@student.aau.dk>
2021-05-10 16:06:10 +0200

    Updated Sensor Parent

--- include/Sensors.h
+++ include/Sensors.h
@@ -3,15 +3,30 @@
 
 #include <string>
 #include <time.h>
+#include <Simulator>
 
 //time_t timer;
 std::string;
 
-class Sensor
+class Sensor //ParentClass
 {
 private:
+    double Value = 0;
+    double inputValue = 0;
+    Simulator &the_connected_sim;
 
 public:
+    Sensor(Simulator &sim) : the_connected_sim(sim)
+    {
+        Value = 5; ///
+    };
+    double getValue() { return the_connected_sim.getTemperature(); };
+    double getInputValue() { return inputValue; };
 };
+
+void getInputValue()
+{
+    GUI.Sim.get
+}
 
 #endif //Sensors
