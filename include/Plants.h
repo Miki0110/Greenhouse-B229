29b57c95a45623310a81d706f8287fbafbd4ece6
gsvend20 <gsvend20@student.aau.dk>
2021-05-10 14:45:28 +0200

    Plant Changes

--- include/Plants.h
+++ include/Plants.h
@@ -10,7 +10,24 @@ std::string;
 class Plant
 {
 private:
+    double lightIdeal = 0;
+    double waterIdeal = 0;
+    double humidityDirtIdeal = 0;
+    double humidityAirIdeal = 0;
+    double pHIdeal = 0;
+    double temperatureIdeal = 0;
+
 public:
+    double getLightIdeal() { return lightIdeal; };
+    double getWaterIdeal() { return waterIdeal; };
+    double getHumidityDirtIdeal() { return humidityDirtIdeal; };
+    double getHumidityAirIdeal() { return humidityAirIdeal; };
+    double getpHIdeal() { return pHIdeal; };
+    double getTemeratureIdeal() { return temperatureIdeal; };
 };
 
 #endif //Plants
+
+/*
+lys vand
+*/
