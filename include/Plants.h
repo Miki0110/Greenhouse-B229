7372d049d8964c12da07e84068dd91f63170c331
gsvend20 <gsvend20@student.aau.dk>
2021-05-10 13:19:48 +0200

    ?

--- include/Plants.h
+++ include/Plants.h
@@ -10,7 +10,24 @@ std::string;
 class Plant
 {
 private:
+    double height = 0.;
+    int Num_Fruits = 0;
+    double growth_rate = 0.;
+    float Water_importance = 1.;
+    float Sun_importance = 1.;
+    float DirtHumindity_importance = 1.;
+
 public:
+    double getHeight() { return height; };
+    int getNumFruits() { return Num_Fruits; };
+    int getGrowth_rate() { return growth_rate; };
+
+    void grow(int tics);
 };
+
+Plant::grow(int tics)
+{
+    height = height + tics * growth_rate;
+}
 
 #endif //Plants
