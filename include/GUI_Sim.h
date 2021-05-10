1846f2d8c0529a976ac3100db0eb9668016c2acb
gsvend20 <gsvend20@student.aau.dk>
2021-05-10 16:05:54 +0200

    Created GUI_Sim

--- include/GUI_Sim.h
+++ include/GUI_Sim.h
@@ -13,4 +13,15 @@ private:
 public:
 };
 
+void CreateSideBar(string name, double minValue, double maxValue, string Color)
+{
+
+    sf::RectangleShape WaterLevelHolder{sf::Vector2f{50., (float)WorldHeight * 8 / 10}};
+    WaterLevelHolder.setFillColor(sf::Color::Transparent);
+    WaterLevelHolder.setOutlineColor(sf::Color(192, 192, 192));
+    WaterLevelHolder.setOutlineThickness(1);
+    WaterLevelHolder.setOrigin(WaterLevelHolder.getSize().x / 2, WaterLevelHolder.getSize().y / 2);
+    WaterLevelHolder.setPosition((WorldLength / 2) + ((WorldLength / 2) / (AccesseriesNumber + 1)), (float)WorldHeight / 2);
+}
+
 #endif //GUI_Sim
