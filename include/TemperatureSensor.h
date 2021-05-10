043faf6eb2e725f6217292d01b8036d94f59b9f9
gsvend20 <gsvend20@student.aau.dk>
2021-05-10 16:06:44 +0200

    Created Temperature Slave

---
+++ include/TemperatureSensor.h
@@ -0,0 +1,27 @@
+#ifndef TemperatureSensor
+#define TemperatureSensor
+
+#include <string>
+#include <time.h>
+#include <Sensors.h>
+
+//time_t timer;
+std::string;
+
+class TemperatureSensor //UnderClass
+{
+private:
+    double Value = 0;
+    double inputValue = 0;
+    Simulator &the_connected_sim;
+
+public:
+    Sensor(Simulator &sim) : the_connected_sim(sim)
+    {
+        Value = 5; ///
+    };
+    double getValue() { return the_connected_sim.getTemperatureValue(); };
+    double getInputValue() { return inputValue; };
+};
+
+#endif //TemperatureSensor
