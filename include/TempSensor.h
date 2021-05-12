5246a6c911e64b88edeaa8840fa7b1de7ea8da1f
gsvend20 <gsvend20@student.aau.dk>
2021-05-12 11:01:42 +0200

    Remade Temp Sensor Class

---
+++ include/TempSensor.h
@@ -0,0 +1,15 @@
+#include <string>
+#include <time.h>
+#include "Simulator.h"
+#include "Sensors.h"
+
+class WaterSensor : public BaseSensor
+{
+private:
+    double Value = 0;
+    double rawMeasurementData;
+    Simulator &the_connected_sim;
+
+public:
+    double measure() override { return the_connected_sim.getTemperatureValue(); };
+};
