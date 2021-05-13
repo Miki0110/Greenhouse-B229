946e66ccefbb7aeefdda2e4c04e335f6e8072133
gsvend20 <gsvend20@student.aau.dk>
2021-05-13 10:44:46 +0200

    Plant recreated

--- include/Plants.h
+++ include/Plants.h
@@ -5,4 +5,18 @@
 class Plant
 {
 private:
+    double lightIdeal = 0;
+    double waterIdeal = 0;
+    double humidityDirtIdeal = 0;
+    double humidityAirIdeal = 0;
+    double pHIdeal = 0;
+    double temperatureIdeal = 0;
 
+public:
+    double getLightIdeal() { return lightIdeal; };
+    double getWaterIdeal() { return waterIdeal; };
+    double getHumidityDirtIdeal() { return humidityDirtIdeal; };
+    double getHumidityAirIdeal() { return humidityAirIdeal; };
+    double getpHIdeal() { return pHIdeal; };
+    double getTemeratureIdeal() { return temperatureIdeal; };
+};
